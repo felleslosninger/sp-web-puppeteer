5194ad14beba6bf5b82c3bf7b6c710732bd85ab2
LRaghawan <linesh.raghawan@difi.no>
2019-12-02 08:37:44 +0100

    Update codecept.conf.js

--- codecept.conf.js
+++ codecept.conf.js
@@ -5,7 +5,7 @@ exports.config = {
     Puppeteer: {
       url :'https://sp-web-test-test1.azurewebsites.net/',
       waitForNavigation:  [ "domcontentloaded", "networkidle0" ],
-      waitForAction: 5000,
+      waitForAction: 10000,
       chrome:{
         args: ['--no-sandbox']
       }
